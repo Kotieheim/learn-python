f2df3b567942e6500dd088bd6b82cbdc8be354de
Dakota Heim <kotieheim@gmail.com>
2020-03-16 17:00:40 -0600

    initial

---
+++ pythontut.py
@@ -0,0 +1,15 @@
+import sys
+import math
+import random
+import threading
+import time
+from functools import reduce
+
+# integers, floats, complex numbers, strings, booleans
+
+t1 = (1, 3.4, "Derek")
+print(len(t1))
+print(t1)
+print(t1[0], t1[-1])
+print(t1[0:2])
+print()
